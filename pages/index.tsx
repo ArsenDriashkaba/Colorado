37564f2c5df1a4084309511be8e5580519eb5d73
ArsenDriashkaba <arsen.dryashkaba.super@gmail.com>
2022-08-16 11:04:37 +0200

    Initial config

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Colorado</title>
+        <meta name="description" content="Generate your own color palette" />
+        <link rel="icon" href="/static/icons/chromatic.png" />
+      </Head>
+
+      <main className={styles.main}></main>
+    </div>
+  );
+};
+
+export default Home;
