4df2211c72037471c7db16833e682a10ac003bb4
ArsenDriashkaba <arsen.dryashkaba.super@gmail.com>
2022-09-12 17:06:30 +0200

    Palette implementation

---
+++ utils/models/palette.ts
@@ -0,0 +1,58 @@
+import { Schema, models, model } from "mongoose";
+
+import {
+  PaletteState,
+  ColorVariant,
+  ColorValue,
+  RGB,
+  HSV,
+  HSL,
+} from "../../types";
+
+interface Palette {
+  name: string;
+  colors: PaletteState;
+}
+
+const rgbSchema = new Schema<RGB>({
+  r: { type: Number, required: true },
+  g: { type: Number, required: true },
+  b: { type: Number, required: true },
+  a: Number,
+});
+
+const hsvSchema = new Schema<HSV>({
+  h: { type: Number, required: true },
+  s: { type: Number, required: true },
+  v: { type: Number, required: true },
+  a: Number,
+});
+
+const hslSchema = new Schema<HSL>({
+  h: { type: Number, required: true },
+  s: { type: Number, required: true },
+  l: { type: Number, required: true },
+  a: Number,
+});
+
+const colorValueSchema = new Schema<ColorValue>({
+  rgb: { type: rgbSchema, required: true },
+  hex: { type: String, required: true },
+  hsv: { type: hsvSchema, required: true },
+  hsl: { type: hslSchema, required: true },
+});
+
+const colorVariantSchema = new Schema<ColorVariant>({
+  value: { type: colorValueSchema, required: true },
+  isLocked: { type: Boolean, required: true },
+  isDark: { type: Boolean, required: true },
+});
+
+const paletteSchema = new Schema<Palette>({
+  name: { type: String, required: true },
+  colors: { type: [colorVariantSchema], required: true },
+});
+
+const Palette = models.Palette || model("Palette", paletteSchema);
+
+export default Palette;
